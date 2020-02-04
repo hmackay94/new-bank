29934eabcc2fc2a2be307c2755c74db446cafadf
Hayley Mackay <hayley.mackay@aol.co.uk>
2020-02-04 18:01:24 +0000

    Initial commit of NewBank code copied into a Maven project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # new-bank
 Advanced Software Engineering Coursework
+
+Language level needs to be set to 7
---
+++ src/main/java/uk/ac/bath/compsci/server/CustomerID.java
@@ -0,0 +1,13 @@
+package uk.ac.bath.compsci.server;
+
+public class CustomerID {
+    private String key;
+
+    public CustomerID(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+}
