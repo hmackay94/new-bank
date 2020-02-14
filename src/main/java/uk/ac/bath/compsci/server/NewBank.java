0b0294bf900b8b557440f74e53baaaa804cf9650
Hayley Mackay <hayley.mackay@aol.co.uk>
2020-02-14 10:12:39 +0000

    Implemented create new account and move between accounts Still to do: - Validation on account before withdraw to new account - Additional test cases

--- src/main/java/uk/ac/bath/compsci/server/NewBank.java
+++ src/main/java/uk/ac/bath/compsci/server/NewBank.java
@@ -1,11 +1,15 @@
 package uk.ac.bath.compsci.server;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+
+import static java.util.Objects.isNull;
 
 public class NewBank {
     private static final NewBank bank = new NewBank();
     private HashMap<String, Customer> customers;
+    public static final Date CURRENT_DATE = new Date();
 
     private NewBank() {
         customers = new HashMap<>();
@@ -13,21 +17,20 @@ public class NewBank {
     }
 
     private void addTestData() {
-        final Date currentDate = new Date();
 
         Customer bhagy = new Customer();
         final double bhagyOpeningBalance = 1000;
-        bhagy.addAccount(new Account("Main", bhagyOpeningBalance, new Transaction(currentDate, "Opening Balance", bhagyOpeningBalance)));
+        bhagy.addAccount(new Account("Main", bhagyOpeningBalance, new Transaction(CURRENT_DATE, "Opening Balance", bhagyOpeningBalance)));
         customers.put("Bhagy", bhagy);
 
         Customer christina = new Customer();
         final double christinaOpeningBalance = 1500;
-        christina.addAccount(new Account("Savings", christinaOpeningBalance, new Transaction(currentDate, "Opening Balance", christinaOpeningBalance)));
+        christina.addAccount(new Account("Savings", christinaOpeningBalance, new Transaction(CURRENT_DATE, "Opening Balance", christinaOpeningBalance)));
         customers.put("Christina", christina);
 
         Customer john = new Customer();
         final double johnOpeningBalance = 250;
-        john.addAccount(new Account("Checking", johnOpeningBalance, new Transaction(currentDate, "Opening Balance", christinaOpeningBalance)));
+        john.addAccount(new Account("Checking", johnOpeningBalance, new Transaction(CURRENT_DATE, "Opening Balance", christinaOpeningBalance)));
         customers.put("John", john);
     }
 
@@ -47,14 +50,18 @@ public class NewBank {
         if (customers.containsKey(customer.getKey())) {
 
             String[] request2 = request.split(" ");
+            Arrays.stream(request2).forEach(System.out::println);
             String cmd = request2[0];
 
             switch (cmd) {
                 case "SHOWMYACCOUNTS":
                     return showMyAccounts(customer);
                 case "NEWACCOUNT":
-                    //TODO - open a new account
-                    return "FAIL";
+                    if (request2.length != 2) {
+                        return "FAIL - invalid command";
+                    } else {
+                        return newAccount(customer, request2[1]);
+                    }
                 case "DEPOSIT":
                     //Deposit money into one of your accounts
                     try {
@@ -69,8 +76,18 @@ public class NewBank {
                     //TODO - Pay a friend (payee)
                     return "FAIL";
                 case "MOVE":
-                    //TODO - move money between your accounts
-                    return "FAIL";
+                    try {
+                        if (request2.length != 4) {
+                            return "FAIL - invalid command";
+                        } else {
+                            return moveMoneyBetweenAccounts(customer,
+                                    Double.parseDouble(request2[1]),
+                                    request2[2],
+                                    request2[3]);
+                        }
+                    } catch (NumberFormatException e) {
+                        return "FAIL - cannot process amount";
+                    }
                 case "PRINTSTATEMENT":
                     //Print a statement of balances and recent transactions to screen
                     String rtn = "FAIL";
@@ -125,4 +142,26 @@ public class NewBank {
         return customerAccount.printTransactions();
     }
 
+    private String newAccount(final CustomerID customerID, final String accountName) {
+        final double openingBalance = 0;
+        final Account newAccount = new Account(accountName, openingBalance,
+                new Transaction(CURRENT_DATE, "Opening Balance", openingBalance));
+        customers.get(customerID.getKey()).addAccount(newAccount);
+        return "SUCCESS";
+    }
+
+    private String moveMoneyBetweenAccounts(final CustomerID customerID, final double amount, final String accountNameFrom, final String accountNameTo) {
+        final Account accountFrom = customers.get(customerID.getKey()).getAccount(accountNameFrom);
+        final Account accountTo = customers.get(customerID.getKey()).getAccount(accountNameTo);
+        if (isNull(accountFrom)) {
+            return "FAIL - Account from does not exist";
+        }
+        if (isNull(accountTo)) {
+            return "FAIL - Account to does not exist";
+        }
+        // TODO: Only do deposit if withdraw was successful
+        accountFrom.withdraw(amount, new Transaction(CURRENT_DATE, "Moved money to " + accountNameTo, amount));
+        accountTo.deposit(amount, new Transaction(CURRENT_DATE, "Moved money from " + accountNameFrom, amount));
+        return "SUCCESS";
+    }
 }
--- src/test/java/uk/ac/bath/compsci/server/NewBankTest.java
+++ src/test/java/uk/ac/bath/compsci/server/NewBankTest.java
@@ -1,5 +1,91 @@
 package uk.ac.bath.compsci.server;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
 class NewBankTest {
 
+    @Test
+    void newAccountWillCreateNewAccount() {
+        final NewBank bank = NewBank.getBank();
+        final CustomerID customerID = mock(CustomerID.class);
+        given(customerID.getKey()).willReturn("John");
+
+        final String actual = bank.processRequest(customerID, "NEWACCOUNT Main");
+
+        assertThat(actual).isEqualTo("SUCCESS");
+    }
+
+    @Test
+    void newAccountWillFailIfCommandIsEmpty() {
+        final NewBank bank = NewBank.getBank();
+        final CustomerID customerID = mock(CustomerID.class);
+        given(customerID.getKey()).willReturn("John");
+
+        final String actual = bank.processRequest(customerID, "NEWACCOUNT");
+
+        assertThat(actual).isEqualTo("FAIL - invalid command");
+    }
+
+    @Disabled("Should be failing but is currently passing")
+    @Test
+    void newAccountWillFailIfAccountAlreadyExists() {
+        final NewBank bank = NewBank.getBank();
+        final CustomerID customerID = mock(CustomerID.class);
+        given(customerID.getKey()).willReturn("John");
+
+        final String actual = bank.processRequest(customerID, "NEWACCOUNT Checking");
+
+        assertThat(actual).isEqualTo("FAIL");
+    }
+
+    @Test
+    void moveWillMoveMoneyBetweenAccounts() {
+        final NewBank bank = NewBank.getBank();
+        final CustomerID customerID = mock(CustomerID.class);
+        given(customerID.getKey()).willReturn("John");
+
+        bank.processRequest(customerID, "NEWACCOUNT Main");
+        final String actual = bank.processRequest(customerID, "MOVE 100 Checking Main");
+
+        assertThat(actual).isEqualTo("SUCCESS");
+    }
+
+    @Test
+    void moveWillFailIfCommandIsEmpty() {
+        final NewBank bank = NewBank.getBank();
+        final CustomerID customerID = mock(CustomerID.class);
+        given(customerID.getKey()).willReturn("John");
+
+        final String actual = bank.processRequest(customerID, "MOVE");
+
+        assertThat(actual).isEqualTo("FAIL - invalid command");
+    }
+
+    @Test
+    void moveWillFailIfNotProvidedValidAmount() {
+        final NewBank bank = NewBank.getBank();
+        final CustomerID customerID = mock(CustomerID.class);
+        given(customerID.getKey()).willReturn("John");
+
+        final String actual = bank.processRequest(customerID, "MOVE a Checking Main");
+
+        assertThat(actual).isEqualTo("FAIL - cannot process amount");
+    }
+
+    @Test
+    void moveWillFailIfBankAccountDoesNotExist() {
+        final NewBank bank = NewBank.getBank();
+        final CustomerID customerID = mock(CustomerID.class);
+        given(customerID.getKey()).willReturn("John");
+
+        bank.processRequest(customerID, "NEWACCOUNT Main");
+        final String actual = bank.processRequest(customerID, "MOVE 100 Checking Savings");
+
+        assertThat(actual).isEqualTo("FAIL - Account to does not exist");
+    }
 }
